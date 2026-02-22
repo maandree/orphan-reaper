18c5dab410d36ef4f5d5cfdf9700cb711b5ba16b
Mattias Andrée <m@maandree.se>
2026-02-22 13:02:55 +0100

    m fixes
    Signed-off-by: Mattias Andrée <m@maandree.se>

--- config.mk
+++ config.mk
@@ -2,8 +2,8 @@ PREFIX     = /usr
 MANPREFIX  = $(PREFIX)/share/man
 LIBEXECDIR = $(PREFIX)/libexec/orphan-reaper
 
-CC = cc
+CC = c99
 
 CPPFLAGS = -D_DEFAULT_SOURCE -D_BSD_SOURCE -D_XOPEN_SOURCE=700
-CFLAGS   = -std=c99 -Wall -O2
+CFLAGS   =
 LDFLAGS  = -s
